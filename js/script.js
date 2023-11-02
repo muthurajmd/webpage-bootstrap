f57f8a8381345652c0589fbd7fe2b150aadb12c7
muthurajmd <muthurajmd1995@gmail.com>
2023-11-02 16:02:45 +0530

    update

--- js/script.js
+++ js/script.js
@@ -13,3 +13,24 @@ $('.play').on('click',function(){
 $('.stop').on('click',function(){
     owl.trigger('stop.owl.autoplay')
 })
+
+let order=document.querySelector(".ss")
+let order1=document.querySelector(".ss1")
+
+let navv=document.getElementById("navbarNavDropdown")
+// const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+
+
+function selectcard() {
+			
+	order.classList.add("collapsed")			
+	order1.classList.add("collapsed")			
+	
+	order.setAttribute("aria-expanded", false)
+	order1.setAttribute("aria-expanded", false)
+	
+	// navv.classList.add("collapse")	
+	// navv.classList.remove("collapsing")	
+	navv.classList.remove("show")	
+
+}
